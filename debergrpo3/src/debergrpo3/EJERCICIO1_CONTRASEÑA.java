9abc0c71e3f14027e4d4232eea0314d30d611e5c
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-05 21:15:25 -0500

    estructura while.Ej1

--- debergrpo3/src/debergrpo3/EJERCICIO1_CONTRASEÑA.java
+++ debergrpo3/src/debergrpo3/EJERCICIO1_CONTRASEÑA.java
@@ -8,10 +8,17 @@ public class EJERCICIO1_CONTRASEÑA {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         final String clave= "ADminUNO";
+        int intentos =0;
         System.out.println("========================="); 
         System.out.println("ADIVINAR CONTRASEÑA");
         System.out.println("=========================");
         System.out.println("INGRESE LA CONTRASEÑA ");
+        String contraTemporal=sc.nextLine();
+        for (;;) {
+            System.out.println("hola");
+            intentos++;
+        if contraTemporal
+        }
         
     }
     
