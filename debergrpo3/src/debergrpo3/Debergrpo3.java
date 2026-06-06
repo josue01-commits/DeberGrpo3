c822aefc00ed07b14370f8b5670ce45138b916fb
Nico <nicojorge666@gmail.com>
2026-06-05 20:43:46 -0500

    nico

---
+++ debergrpo3/src/debergrpo3/Debergrpo3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package debergrpo3;
+
+/**
+ *
+ * @author Nikholov
+ */
+public class Debergrpo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("hola grupo3");
+    }
+    
+}
