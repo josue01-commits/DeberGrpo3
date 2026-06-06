5e51283c56cde4eb1f27fb856aa24c8f365df17f
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-05 20:52:48 -0500

    estructura del program.Ej1

---
+++ GRUPO 3/src/grupo/pkg3/GRUPO3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo.pkg3;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class GRUPO3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
