1d87938809e2aa1e966ba1eb22edb2bba6234667
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-05 20:59:00 -0500

    definir la variable fija,importar Scanner y mensajes.Ej1

---
+++ debergrpo3/src/debergrpo3/EJERCICIO1_CONTRASEÑA.java
@@ -0,0 +1,18 @@
+
+package debergrpo3;
+import java.util.Scanner;
+
+public class EJERCICIO1_CONTRASEÑA {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        final String clave= "ADminUNO";
+        System.out.println("========================="); 
+        System.out.println("ADIVINAR CONTRASEÑA");
+        System.out.println("=========================");
+        System.out.println("INGRESE LA CONTRASEÑA ");
+        
+    }
+    
+}
