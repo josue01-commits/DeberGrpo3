e688c20446440698dc8ebf7d3634d9c2becd83cf
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-07 09:47:02 -0500

    Diseño del menú principal

--- debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
+++ debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
@@ -11,7 +11,17 @@ public class KEVIN_CHINGO {
      int opcion;
      do{
          
-         
+         System.out.println("""
+        =========================================
+         MENU PRINCIPAL
+        =========================================
+        1. Contar números pares e impares
+        2. Registrar ventas diarias de una tienda
+        3. Contar números hasta ingresar uno negativo
+        4. Generar tabla de multiplicar del 1 al 20
+        0. Salir
+        =========================================
+        """);
 
      }while(opcion!=0);
 }  
