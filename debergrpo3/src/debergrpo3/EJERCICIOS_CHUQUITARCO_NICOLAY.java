86a7adf15aabde013b55f445d606fc722c016f5e
Nico <nicojorge666@gmail.com>
2026-06-06 20:16:31 -0500

    EJERCICIO 10

--- debergrpo3/src/debergrpo3/EJERCICIOS_CHUQUITARCO_NICOLAY.java
+++ debergrpo3/src/debergrpo3/EJERCICIOS_CHUQUITARCO_NICOLAY.java
@@ -21,3 +21,20 @@ public static void ejercicio9(){
         System.out.println("El factorial de " + numero + " es: " + factorial);
     }
  }
+
+public static void ejercicio10(){
+    //DETRERMINAR CUALES SON POSITIVOS, NEGATIVOS Y CEROS
+    System.out.println("===============");
+    System.out.println("Que numero es ?"); 
+        
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
+        //cliclo for para ingresar los 15
+        for (int i = 1; i <= 15; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            int numero = sc.nextInt();
+        }
+
+
+}
