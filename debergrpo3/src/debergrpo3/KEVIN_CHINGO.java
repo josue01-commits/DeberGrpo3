820bb538f506ee16d1792fb411ff62132e3632e6
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-07 09:39:09 -0500

    Creación del método principal

--- debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
+++ debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
@@ -4,5 +4,8 @@ package debergrpo3;
 import java.util.Scanner;
 public class KEVIN_CHINGO {
     static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+}
     
 }
