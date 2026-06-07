8085ef63d59d514c446de8d9bf6fd7ff602445b7
kevinchingo2458-star <kevin.chingo2458@utc.edu.ec>
2026-06-07 09:37:23 -0500

    Inicialización del Scanner

--- debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
+++ debergrpo3/src/debergrpo3/KEVIN_CHINGO.java
@@ -1,10 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package debergrpo3;
 
 import java.util.Scanner;
 public class KEVIN_CHINGO {
+    static Scanner sc = new Scanner(System.in);
     
 }
