ad5c2ecaa87a36c3bc939c2c6d980ec1030efc30
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 22:29:17 -0500

    calculadora de salario

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -12,33 +12,16 @@ import java.util.Scanner;
  */
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
-         //conversion de temperatura
-         
+         // calculadora de salario
          try (Scanner leer = new Scanner(System.in)) {
-             double celsius;
-             double fahrenheit;
-             double kelvin;
-             
-             System.out.println("===== CONVERSOR DE TEMPERATURAS =====");
-             
-             System.out.print("Ingrese la temperatura en grados Celsius: ");
-             celsius = leer.nextDouble();
-             
-             fahrenheit = (celsius * 9 / 5) + 32;
-             kelvin = celsius + 273.15;
-             
-             System.out.println("\nRESULTADOS");
-             System.out.println("Celsius: " + celsius + " °C");
-             System.out.println("Fahrenheit: " + fahrenheit + " °F");
-             System.out.println("Kelvin: " + kelvin + " K");
-             
-             if (celsius < 0) {
-                 System.out.println("La temperatura está bajo cero.");
-             } else if (celsius > 30) {
-                 System.out.println("La temperatura es alta.");
-             } else {
-                 System.out.println("La temperatura es moderada.");
-           }}
+             double horas, pago, salario;
+             System.out.print("Ingrese las horas trabajadas: ");
+             horas = leer.nextDouble();
+             System.out.print("Ingrese el pago por hora: ");
+             pago = leer.nextDouble();
+             salario = horas * pago;
+             System.out.println("Salario total: $" + salario);
+         }
     }
 }
 
