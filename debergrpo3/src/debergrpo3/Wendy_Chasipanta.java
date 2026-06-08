51fc7e61fd1db36afcdb8acca4aba2a033e3452d
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 23:17:22 -0500

    descuento del 10 porciento

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -12,26 +12,27 @@ import java.util.Scanner;
  */
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
-         //agregando nombre
+         
          try (Scanner leer = new Scanner(System.in)) {
              String cliente;
              int opcion;
              int cantidad;
              double total = 0;
+             double descuento = 0;
              
-             System.out.println("***** RESTAURANTE *******");
+             System.out.println("****** RESTAURANTE ******");
              
              System.out.print("Ingrese el nombre del cliente: ");
              cliente = leer.nextLine();
              
              do {
-                 System.out.println("\n****** MENU *****");
+                 System.out.println("\n****** MENU ******");
                  System.out.println("1. Hamburguesa ($5)");
                  System.out.println("2. Pizza ($8)");
                  System.out.println("3. Gaseosa ($2)");
                  System.out.println("4. Ver factura");
                  System.out.println("5. Salir");
-                 System.out.print("Seleccione una opcion: ");
+                 System.out.print("Seleccione una opción: ");
                  opcion = leer.nextInt();
                  
                  switch (opcion) {
@@ -55,14 +56,22 @@ public class Wendy_Chasipanta {
                      }
                          
                      case 4 -> {
+                         if (total > 20) {
+                             descuento = total * 0.10;
+                         } else {
+                             descuento = 0;
+                         }
+                         
                          System.out.println("\n===== FACTURA =====");
                          System.out.println("Cliente: " + cliente);
-                         System.out.println("Total a pagar: $" + total);
+                         System.out.println("Subtotal: $" + total);
+                         System.out.println("Descuento: $" + descuento);
+                         System.out.println("Total a pagar: $" + (total - descuento));
                      }
                          
                      case 5 -> System.out.println("Gracias por su compra.");
                          
-                     default -> System.out.println("Opcion no valida.");
+                     default -> System.out.println("Opción no válida.");
                  }
                  
              } while (opcion != 5);
