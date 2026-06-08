12aafe21db7cc611323f8f1eac1a2592a44e7a0e
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 23:24:48 -0500

    nombre del dueño al registro de mascota

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -12,10 +12,11 @@ import java.util.Scanner;
  */
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
-         //registro de mascota
+         //anombre del dueño
          try (Scanner leer = new Scanner(System.in)) {
              String nombreMascota;
              String especie;
+             String nombreDueno;
              int edad;
              
              System.out.println(" REGISTRO DE MASCOTAS ");
@@ -26,12 +27,16 @@ public class Wendy_Chasipanta {
              System.out.print("Ingrese la especie: ");
              especie = leer.nextLine();
              
-             System.out.print("Ingrese la edad: ");
+             System.out.print("Ingrese el nombre del dueño: ");
+             nombreDueno = leer.nextLine();
+             
+             System.out.print("Ingrese la edad de la mascota: ");
              edad = leer.nextInt();
              
-             System.out.println("\n DATOS DE LA MASCOTA ");
-             System.out.println("Nombre: " + nombreMascota);
+             System.out.println("\n===== DATOS DE LA MASCOTA =====");
+             System.out.println("Nombre de la mascota: " + nombreMascota);
              System.out.println("Especie: " + especie);
+             System.out.println("Dueño: " + nombreDueno);
              System.out.println("Edad: " + edad + " años");
          }
     }
