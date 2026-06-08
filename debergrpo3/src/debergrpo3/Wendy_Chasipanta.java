dd230561a537a99193e6f7204992fd07cef85526
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 22:44:56 -0500

    control unitario

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -12,38 +12,34 @@ import java.util.Scanner;
  */
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
-         //sistema de biblioteca
+        //control unitario
          try (Scanner leer = new Scanner(System.in)) {
-             String nombreLibro;
-             String autor;
-             int diasRetraso;
-             double multa = 0;
+             int cantidad;
+             double precio;
+             double valorTotal = 0;
              
-             System.out.println("****** SISTEMA DE BIBLIOTECA ********");
+             System.out.print("Cuantos productos desea registrar?: ");
+             int n = leer.nextInt();
+             leer.nextLine();
              
-             System.out.print("Ingrese el nombre del libro: ");
-             nombreLibro = leer.nextLine();
-             
-             System.out.print("Ingrese el nombre del autor: ");
-             autor = leer.nextLine();
-             
-             System.out.print("Ingrese los dias de retraso en la devolucion: ");
-             diasRetraso = leer.nextInt();
-             
-             if (diasRetraso > 0) {
-                 multa = diasRetraso * 0.50;
-             } 
-             System.out.println("\n****** REPORTE ******");
-             System.out.println("Libro: " + nombreLibro);
-             System.out.println("Autor: " + autor);
-             System.out.println("Dias de retraso: " + diasRetraso);
-             System.out.println("Multa: $" + multa);
-             
-             if (multa > 0) {
-                 System.out.println("El usuario debe pagar una multa.");
-             } else {
-                 System.out.println("El libro fue devuelto a tiempo.");
-             }}
+             for (int i = 1; i <= n; i++) {
+                 
+                 System.out.println("\nProducto #" + i);
+                 
+                 System.out.print("Nombre: ");
+                 leer.nextLine();
+                 
+                 System.out.print("Cantidad: ");
+                 cantidad = leer.nextInt();
+                 
+                 System.out.print("Precio unitario: ");
+                 precio = leer.nextDouble();
+                 leer.nextLine();
+                 
+                 valorTotal += cantidad * precio;
+             }
+             System.out.println("\nValor total del inventario: $" + valorTotal);
+         }
     }
 }
 
