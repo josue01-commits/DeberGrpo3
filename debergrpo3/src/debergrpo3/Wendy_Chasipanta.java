6a963a2609b707b7db97aaab87710ca5cea9e7d7
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 23:09:31 -0500

    cambio, agregar nombre del cliente

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -12,18 +12,24 @@ import java.util.Scanner;
  */
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
-         //sistema de restaurante
+         //agregando nombre
          try (Scanner leer = new Scanner(System.in)) {
+             String cliente;
              int opcion;
              int cantidad;
              double total = 0;
              
+             System.out.println("***** RESTAURANTE *******");
+             
+             System.out.print("Ingrese el nombre del cliente: ");
+             cliente = leer.nextLine();
+             
              do {
-                 System.out.println("\n===== RESTAURANTE =====");
+                 System.out.println("\n****** MENU *****");
                  System.out.println("1. Hamburguesa ($5)");
                  System.out.println("2. Pizza ($8)");
                  System.out.println("3. Gaseosa ($2)");
-                 System.out.println("4. Ver total");
+                 System.out.println("4. Ver factura");
                  System.out.println("5. Salir");
                  System.out.print("Seleccione una opcion: ");
                  opcion = leer.nextInt();
@@ -31,28 +37,32 @@ public class Wendy_Chasipanta {
                  switch (opcion) {
                      
                      case 1 -> {
-                         System.out.print("Cantidad: ");
+                         System.out.print("Cantidad de hamburguesas: ");
                          cantidad = leer.nextInt();
                          total += cantidad * 5;
                      }
-                     
+                         
                      case 2 -> {
-                         System.out.print("Cantidad: ");
+                         System.out.print("Cantidad de pizzas: ");
                          cantidad = leer.nextInt();
                          total += cantidad * 8;
                      }
-                     
+                         
                      case 3 -> {
-                         System.out.print("Cantidad: ");
+                         System.out.print("Cantidad de gaseosas: ");
                          cantidad = leer.nextInt();
                          total += cantidad * 2;
                      }
-                     
-                     case 4 -> System.out.println("Total a pagar: $" + total);
-                     
+                         
+                     case 4 -> {
+                         System.out.println("\n===== FACTURA =====");
+                         System.out.println("Cliente: " + cliente);
+                         System.out.println("Total a pagar: $" + total);
+                     }
+                         
                      case 5 -> System.out.println("Gracias por su compra.");
-                     
-                     default -> System.out.println("Opción invalida.");
+                         
+                     default -> System.out.println("Opcion no valida.");
                  }
                  
              } while (opcion != 5);
