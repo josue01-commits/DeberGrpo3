a2259a5b4c04d4189db821451371707d034bf979
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-07 21:33:47 -0500

    ingresar solo numeros positivos

--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -13,17 +13,39 @@ import java.util.Scanner;
 public class Wendy_Chasipanta {
      public static void main(String[] args) {
 
-         try (Scanner leer = new Scanner(System.in)) {
+        try (Scanner leer = new Scanner(System.in)) {
+            //INGRESO DE NUMEROS POSITIVOS 
+             int numero;
              int suma = 0;
+             int cantidad = 0;
+             double promedio;
              
-             for (int i = 1; i <= 5; i++) {
-                 System.out.print("Ingrese un numero " + i + ": ");
-                 int numero = leer.nextInt();
-                 suma += numero;
+             System.out.println("INGRESO DE NUMEROS POSITIVOS");
+             System.out.println("El proceso termina al ingresar un numero negativo.");
+             
+             System.out.print("Ingrese un numero: ");
+             numero = leer.nextInt();
+             
+             while (numero >= 0) {
+                 
+                 suma = suma + numero;
+                 cantidad++;
+                 
+                 System.out.print("Ingrese otro numero: ");
+                 numero = leer.nextInt();
              }
              
-             System.out.println("La suma total es: " + suma);
-         }
-    
+             if (cantidad > 0) {
+                 promedio = (double) suma / cantidad;
+                 
+                 System.out.println("\nRESULTADOS");
+                 System.out.println("Cantidad de numeros ingresados: " + cantidad);
+                 System.out.println("Suma total: " + suma);
+                 System.out.println("Promedio: " + promedio);
+             } else {
+                 System.out.println("No se ingresaron numeros positivos.");
+             }}
+        }
     }
-}
+    
+    
