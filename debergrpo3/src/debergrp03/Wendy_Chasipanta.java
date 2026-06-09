797c2b2e7ba3c1d0688c86b19c6b9f0c806cba47
wendychasipanta3175 <wendy.chasipanta3175@utc.edu.ec>
2026-06-08 19:48:44 -0500

    Ejercicios del deber: 13,14,15

--- debergrpo3/src/debergrp03/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrp03/Wendy_Chasipanta.java
@@ -11,15 +11,16 @@ public class Wendy_Chasipanta {
 
     public static void main(String[] args) {
 
-        try (Scanner leer = new Scanner(System.in)) {
-            System.out.print("Ingrese un número: ");
-            int numero = leer.nextInt();
-            
-            System.out.println("Tabla de multiplicar del " + numero);
-            
-            for (int i = 1; i <= 10; i++) {
-                System.out.println(numero + " x " + i + " = " + (numero * i));
+        int suma = 0;
+
+        for (int i = 0; i <= 2; i++) {
+
+            if (i % 2 == 0) {
+                suma = suma + i;
             }
+
         }
+
+        System.out.println("La suma es: " + suma);
     }
-}
+    }
--- debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
+++ debergrpo3/src/debergrpo3/Wendy_Chasipanta.java
@@ -11,30 +11,125 @@ import java.util.Scanner;
  * @author USER
  */
 public class Wendy_Chasipanta {
-     public static void main(String[] args) {
-         //algoritmo que solicite un numero entero y determine si es primo
-         try (Scanner leer = new Scanner(System.in)) {
-             int numero;
-             int contador = 0;
-             
-             System.out.println("Ingrese un numero entero: ");
-             numero = leer.nextInt();
-             
-            for (int i = 1; i <= numero; i++) {
+    static Scanner sc = new Scanner(System.in);
+    
+        public static void menu(){
+            // MENU DE OPCIONES
+        int opcion;
+        do{
+            System.out.println("""
+                            MENU DE OPCIONES
+                            1. Ejercicio13
+                            2. ejercicio14
+                            3. ejercicio15
+                            4. Salir  
+                            """);
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1 -> ejercicio13();
+                case 2 -> ejercicio14();
+                case 3 -> ejercicio15();
+                case 4 -> System.out.println("SALIENDO DEL SISTEMA...");
+                default -> System.out.println("Opcion invalida.");
+            }
+        } while (opcion != 4);
+        sc.close();
+            
+        }
+        
+    // EJERCICIO 13
+    // Solicitar 10 numeros y mostrar el mayor y el menor
+    public static void ejercicio13() {
 
-            if (numero % i == 0) {
-                contador++;
+        int num;
+        int mayor = 0;
+        int menor = 0;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print("Ingrese el numero " + i + ": ");
+            num = sc.nextInt();
+
+            if (i == 1) {
+                mayor = num;
+                menor = num;
+            }
+
+            if (num > mayor) {
+                mayor = num;
+            }
+
+            if (num < menor) {
+                menor = num;
             }
         }
-        if (contador == 2) {
-            System.out.println("El numero ingresado es primo.");
-        } else {
-            System.out.println("El numero ingresado no es primo.");
+
+        System.out.println("El numero mayor es: " + mayor);
+        System.out.println("El numero menor es: " + menor);
+    }
+    
+    // EJERCICIO 14
+    // Sumar números pares entre 1 y 200
+    public static void ejercicio14() {
+
+        int suma = 0;
+
+        for (int i = 1; i <= 200; i++) {
+
+            if (i % 2 == 0) {
+                suma += i;
+            }
         }
-        leer.close();
-         }
+        
+        
+        System.out.println("La suma de los numeros pares es: " + suma);
+    }
+    
+    
+    
+    // EJERCICIO 15
+    // Verificar si un número es primo
+    public static void ejercicio15() {
+
+        int numero;
+        boolean primo = true;
+
+        System.out.print("Ingrese un numero entero: ");
+        numero = sc.nextInt();
+
+        if (numero <= 1) {
+
+            primo = false;
+
+        } else {
+
+            for (int i = 2; i < numero; i++) {
+
+                if (numero % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        }
+
+        if (primo) {
+            System.out.println("El numero si es primo");
+        } else {
+            System.out.println("El numero no es primo");
+        }
+    }
+    
+    
+
+    // Menu
+    public static void main(String[] args) {
+
+        menu();
     }
 }
+    
+
 
     
     
